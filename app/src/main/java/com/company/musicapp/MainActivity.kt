c5a6387d3df57d7bf6f359270a86ba51ace02ac0
kim <s158614@nishitech.ac.jp>
2020-05-30 19:46:10 +0900

    버튼클릭 메인-세컨드 작용

--- app/src/main/java/com/company/musicapp/MainActivity.kt
+++ app/src/main/java/com/company/musicapp/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.company.musicapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+
 
 class MainActivity : AppCompatActivity() {
 
+    fun goSecondView(view: View) {
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
+    }
+
+    // 뷰가 생성 되엇을 때
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 그릴 xml 파일을 연결 시켜준다. 즉 설정한다.
         setContentView(R.layout.activity_main)
     }
+
 }
